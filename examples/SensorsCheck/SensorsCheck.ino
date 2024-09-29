8a3ac10cc4c183834403b9bb90a4d64da6d663af
Giulio Pons <pons@rockit.it>
2024-09-29 23:12:24 +0200

    added example for sensors reading

---
+++ examples/SensorsCheck/SensorsCheck.ino
@@ -0,0 +1,33 @@
+#include <Penguin.h>
+
+Penguin robot;
+
+void setup()
+{
+  Serial.begin(9600);  
+  robot.servoInit();
+  robot.servoAttach();
+  robot.homes(200);
+  robot.servoDetach();  
+}
+
+void loop()
+{
+  
+  int irL = robot.irLeft();
+  int irR = robot.irRight();
+  int distance = robot.getDistance();
+  double bat = robot.batteryLevel();
+  
+  Serial.print("IR LEFT: ");
+  Serial.print(irL);
+  Serial.print(" IR RIGHT: ");
+  Serial.print(irR);
+  Serial.print(" DISTANCE: ");
+  Serial.print(distance);
+  Serial.print(" BATTERY: ");
+  Serial.print(bat);
+  Serial.println();
+
+  delay(1000);
+}
